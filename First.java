2e6e31a21a6c6654249e78d19843a586aaaead16
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-05-26 20:34:08 +0500

    first frame

--- First.java
+++ First.java
@@ -1,46 +1,12 @@
+import javax.swing.JFrame;
+
 public class First{
-    int shah;
-
+    public static void main(String[] args) {
+        JFrame j = new JFrame();
+        j.setSize(300, 300);
+        j.setVisible(true);
+        
+        
+        
+    }
 }
-    public First() {
-    }
-
-    public First(int shah) {
-        this.shah = shah;
-    }
-
-    public int getShah() {
-        return this.shah;
-    }
-
-    public void setShah(int shah) {
-        this.shah = shah;
-    }
-
-    public First shah(int shah) {
-        setShah(shah);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof First)) {
-            return false;
-        }
-        First first = (First) o;
-        return shah == first.shah;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(shah);
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " shah='" + getShah() + "'" +
-            "}";
-    }
